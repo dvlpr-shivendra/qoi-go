6032771f91e55e0a5b280b6ddc03373795f981a8
Shivendra Kumar <shivendratechster@gmail.com>
2025-01-04 19:46:56 +0530

    gtk setup

--- go.mod
+++ go.mod
@@ -2,23 +2,4 @@ module github.com/dvlpr-shivendra/qoi-decoder
 
 go 1.23.4
 
-require github.com/faiface/pixel v0.10.0
-
-require (
-	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
-	google.golang.org/protobuf v1.35.1 // indirect
-)
-
-require (
-	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
-	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
-	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20191125211704-12ad95a8df72 // indirect
-	github.com/go-gl/mathgl v0.0.0-20190416160123-c4601bc793c7 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff // indirect
-	google.golang.org/grpc v1.69.2
-)
+require github.com/gotk3/gotk3 v0.6.3
--- main.go
+++ main.go
@@ -1,45 +1,76 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net"
+	"log"
 
-	"github.com/dvlpr-shivendra/qoi-decoder/service" // Adjust the import path accordingly
-	"google.golang.org/grpc"
+	"github.com/gotk3/gotk3/cairo"
+	"github.com/gotk3/gotk3/gtk"
 )
 
-type grpcHandler struct {
-	service.UnimplementedBackendServiceServer
-}
-
-// Implement TestConnection method
-func (h *grpcHandler) TestConnection(ctx context.Context, in *service.Empty) (*service.Message, error) {
-	return &service.Message{Message: "Connection successful"}, nil
-}
-
 func main() {
-	// Initialize gRPC server
-	grpcServer := grpc.NewServer()
+	// Initialize GTK
+	gtk.Init(nil)
 
-	// Listen on localhost:2000
-	l, err := net.Listen("tcp", "localhost:2000")
+	// Create a window
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to create window:", err)
 	}
-	defer l.Close()
+	win.SetTitle("Render Pixels with Timer")
+	win.SetDefaultSize(1280, 720)
 
-	// Register BackendServiceServer with the gRPC server
-	service.RegisterBackendServiceServer(grpcServer, &grpcHandler{})
+	// Handle window close
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
 
-	fmt.Println("Starting gRPC server on localhost:2000")
-
-	// Start serving gRPC requests
-	if err := grpcServer.Serve(l); err != nil {
-		panic(err)
+	// Create a vertical box container
+	box, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 5)
+	if err != nil {
+		log.Fatal("Failed to create box:", err)
 	}
 
-	// QoifImage processing
-	qoifImage := NewQoif("./qoi_test_images/kodim23.qoi")
-	qoifImage.Process()
+	// Create a drawing area
+	area, err := gtk.DrawingAreaNew()
+	if err != nil {
+		log.Fatal("Failed to create drawing area:", err)
+	}
+	area.SetSizeRequest(1280, 720)
+
+	// Connect the draw signal to render the pixels
+	area.Connect("draw", func(da *gtk.DrawingArea, cr *cairo.Context) {
+
+		qoif := NewQoif("./qoi_test_images/dice.qoi")
+
+		err = qoif.Process()
+
+		for y, scanline := range qoif.scanlines {
+			for x, pixel := range scanline {
+				cr.SetSourceRGBA(
+					float64(pixel.r)/255.0,
+					float64(pixel.g)/255.0,
+					float64(pixel.b)/255.0,
+					1,
+				)
+				cr.Rectangle(float64(x*100), float64(y*100), 100, 100)
+				cr.Fill()
+			}
+		}
+
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	// Add the drawing area and label to the box
+	box.PackStart(area, false, false, 0)
+
+	// Add the box to the window
+	win.Add(box)
+
+	// Show all widgets
+	win.ShowAll()
+
+	// Run GTK main loop
+	gtk.Main()
 }
