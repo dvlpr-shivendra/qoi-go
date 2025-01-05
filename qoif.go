a237e6c880c387747a4357519160dab91dc1c0db
Shivendra Kumar <shivendratechster@gmail.com>
2025-01-05 19:28:17 +0530

    remove logs

--- qoif.go
+++ qoif.go
@@ -91,18 +91,15 @@ func (q *Qoif) PopulateScanlines(streamer *Streamer) error {
 
 		scaneline := make([]Pixel, q.header.width)
 		for j := 0; j < len(scaneline); j++ {
-			fmt.Printf("Reading data for pixel (%d, %d) \n", i, j)
 			if run > 0 {
 				run--
 			} else {
 				chunk, err := streamer.StreamUInt8()
-				fmt.Printf("Chunk binary: %08b\n", chunk)
 				if err != nil {
 					return err
 				}
 
 				if chunk == QOI_OP_RGB {
-					fmt.Println("Processing QOI_OP_RGB")
 					pixel.r, err = streamer.StreamUInt8()
 					if err != nil {
 						return err
@@ -117,7 +114,6 @@ func (q *Qoif) PopulateScanlines(streamer *Streamer) error {
 					}
 					pixel.a = 255
 				} else if chunk == QOI_OP_RGBA {
-					fmt.Println("Processing QOI_OP_RGBA")
 					pixel.r, err = streamer.StreamUInt8()
 					if err != nil {
 						return err
@@ -135,15 +131,12 @@ func (q *Qoif) PopulateScanlines(streamer *Streamer) error {
 						return err
 					}
 				} else if (chunk & QOI_MASK_2) == QOI_OP_INDEX {
-					fmt.Println("Processing QOI_OP_INDEX")
 					*pixel = index[chunk]
 				} else if (chunk & QOI_MASK_2) == QOI_OP_DIFF {
-					fmt.Println("Processing QOI_OP_DIFF")
 					pixel.r += ((chunk >> 4) & 0x03) - 2
 					pixel.g += ((chunk >> 2) & 0x03) - 2
 					pixel.b += (chunk & 0x03) - 2
 				} else if (chunk & QOI_MASK_2) == QOI_OP_LUMA {
-					fmt.Println("Processing QOI_OP_LUMA")
 					b2, err := streamer.StreamUInt8()
 					if err != nil {
 						return err
@@ -153,7 +146,6 @@ func (q *Qoif) PopulateScanlines(streamer *Streamer) error {
 					pixel.g += vg
 					pixel.b += vg - 8 + (b2 & 0x0f)
 				} else if (chunk & QOI_MASK_2) == QOI_OP_RUN {
-					fmt.Println("Processing QOI_OP_RUN")
 					run = int((chunk & 0x3f))
 				}
 				index[colorHash(*pixel)%64] = *pixel
